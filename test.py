4ff631e7e6e2b9db5360fa00c6e237349e2901b9
Yunzhi Gao <gaoyunzhi@gmail.com>
2023-01-30 19:30:40 -0800

    commit

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import twitter_batch_block
 import plain_db
 import time
+from twitter_batch_block import *
 
 to_block = plain_db.load('to_block')
 
@@ -8,5 +9,21 @@ def test():
 	for link, target in to_block.items.items():
 		twitter_batch_block.block(link, target)
 
+def testSingleBlock():
+	client = tweepy.Client(
+        bearer_token=credential['bearer_token'],
+        consumer_key=credential['consumer_key'],
+        consumer_secret=credential['consumer_secret'],
+        access_token=credential['access_key'],
+        access_token_secret=credential['access_secret'])
+	me = client.get_user(username=credential['main_user']).data
+	me_followering = client.get_users_following(me.id).data
+	target_user = client.get_user(username=credential['test_target_user']).data
+	# print('me', me)
+	# print('target_user', target_user)
+	twitter_batch_block.singleBlock(client, target_user, me_followering)
+
 if __name__ == '__main__':
-	test()
+	# test()
+	# testSingleBlock()
+	load_db_all()
--- twitter_batch_block.py
+++ twitter_batch_block.py
@@ -11,13 +11,75 @@ with open('credential') as f:
     credential = yaml.load(f, Loader=yaml.FullLoader)
 existing = plain_db.loadKeyOnlyDB('existing')
 
+timer = {}
+def wait(key, sec):
+    time.sleep(max(0, timer.get(key, 0) - time.time()))
+    timer[key] = time.time() + sec
+
+def prepare_dbs():
+    global followers_db, following_db
+    followers_db = {}
+    following_db = {}
+    for user in credential['users'].items():
+        followers_db = plain_db.loadKeyOnlyDB(user + '_followers')
+        followering_db = plain_db.loadKeyOnlyDB(user + '_followering')
+
+def prepare_clients():
+    global clients
+    clients = {}
+    for user, setting in credential['users'].items():
+        clients[user] = client = tweepy.Client(
+            bearer_token=credential['bearer_token'],
+            consumer_key=credential['consumer_key'],
+            consumer_secret=credential['consumer_secret'],
+            access_token=setting['access_key'],
+            access_token_secret=setting['access_secret'])
+
+def load_single(me, db_name, f):
+    db = plain_db.loadKeyOnlyDB(db_name)
+    wait(str(f), 60)
+    result = f(me.id)
+    while result.data:
+        print(len(result.data))
+        for user in result.data:
+            plain_db.add(user.username)
+        token = result.meta.next_token
+        if not token:
+            return
+        wait(str(f), 60)
+        result = f(me.id, pagination_token=token)
+        
+def load_dbs(user, client):
+    suffix_to_method = {
+        'followers': client.get_users_followers,
+        'followering': client.get_users_following}
+    me = client.get_user(username=user).data
+    for suffix, f in suffix_to_method.items():
+        load_single(me, user + '_' + suffix, f)
+
+def load_db_all():
+    prepare_clients()
+    for user, client in clients.items():
+        load_dbs(user, client)
+
 def get_intersection(list1, list2):
     set1 = set([x.username for x in list1])
     set2 = set([x.username for x in list2])
+    print('set1', len(set1), set1)
+    print('set2', len(set2), set2)
     return set1 & set2
 
 def getLink(x):
     return 'https://twitter.com/' + x
+
+def singleBlock(client, user, me_followering):
+    followers = client.get_users_followers(user.id).data or []
+    intersection = get_intersection(me_followering, followers)
+    if intersection:
+        print(' '.join([toLink(x) for x in user.username + list(intersection)]))
+    else:
+        ...
+        # tele_target.send_message(user.username)
 
 def block(link, target):
     tele_target = bot.get_chat(target)
@@ -36,10 +98,6 @@ def block(link, target):
         if existing.contain(user.username):
             continue
         time.sleep(120)
-        followers = client.get_users_followers(user.id).data or []
-        intersection = get_intersection(me_followering, followers)
-        if intersection:
-            print(' '.join([toLink(x) for x in user.username + list(intersection)]))
-        else:
-            tele_target.send_message(user.username)
+        singleBlock(client, user, me_followering)
         existing.add(user.username)
+        
